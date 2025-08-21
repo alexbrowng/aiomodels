72d3403d5fd22a88d5938d794c2f934fbd85933e
Alejandro Castaño González <unocongafas@gmail.com>
2025-08-21 19:44:44 +0200

    feat: add user tool calls

--- src/aiomodels/messages/user_message.py
+++ src/aiomodels/messages/user_message.py
@@ -5,11 +5,13 @@ from aiomodels.contents.audio_content import AudioContent
 from aiomodels.contents.document_content import DocumentContent
 from aiomodels.contents.image_content import ImageContent
 from aiomodels.contents.text_content import TextContent
+from aiomodels.tools.tool_call import ToolCall
 
 
 @dataclasses.dataclass(frozen=True, slots=True)
 class UserMessage:
     content: str | typing.Sequence[TextContent | ImageContent | DocumentContent | AudioContent]
+    tool_calls: typing.Sequence[ToolCall] | None = None
     name: str | None = None
     role: typing.Literal["user"] = "user"
 
