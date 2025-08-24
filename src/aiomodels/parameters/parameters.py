9635c547666b5ce261a78e1b28a37736af302073
Alejandro Castaño González <unocongafas@gmail.com>
2025-08-24 18:59:24 +0200

    feat: add more parameters

--- src/aiomodels/parameters/parameters.py
+++ src/aiomodels/parameters/parameters.py
@@ -6,9 +6,12 @@ class Parameters:
     max_tokens: int | None = None
     temperature: float | None = None
     top_p: float | None = None
+    top_k: int | None = None
     frequency_penalty: float | None = None
+    repetition_penalty: float | None = None
     presence_penalty: float | None = None
+    seed: int | None = None
     stop: list[str] | None = None
 
     def __str__(self) -> str:
-        return f"Parameters(max_tokens={self.max_tokens}, temperature={self.temperature}, top_p={self.top_p}, frequency_penalty={self.frequency_penalty}, presence_penalty={self.presence_penalty}, stop={self.stop})"
+        return f"Parameters(max_tokens={self.max_tokens}, temperature={self.temperature}, top_p={self.top_p}, top_k={self.top_k}, frequency_penalty={self.frequency_penalty}, repetition_penalty={self.repetition_penalty}, presence_penalty={self.presence_penalty}, seed={self.seed}, stop={self.stop})"
